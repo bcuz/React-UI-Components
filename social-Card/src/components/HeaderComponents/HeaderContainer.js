194750940854cafaf141929ff578c6b81ab85ba8
adam <bcuz@users.noreply.github.com>
2019-02-27 10:30:55 -0500

    get stuff to render

--- social-Card/src/components/HeaderComponents/HeaderContainer.js
+++ social-Card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail';
+
+const HeaderContainer = () => {
+  return <ImageThumbnail />
+}
+
+export default HeaderContainer;
