fc405014177d7623437555a0c9d2deba6e480d07
adam <bcuz@users.noreply.github.com>
2019-02-27 21:07:11 -0500

    card content

--- social-Card/src/components/CardComponents/CardContainer.js
+++ social-Card/src/components/CardComponents/CardContainer.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+function handleClick(e) {
+  e.preventDefault();
+  window.open('http://www.reactjs.org', '_blank');
+}
 
 const CardContainer = () => {
   return (
-    <div className='card'>
+    <div className='card' onClick={handleClick}>
       <CardBanner />
+      <CardContent />
     </div>
   )
 }
