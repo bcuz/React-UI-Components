28d88114d8a06b309bf156fea41ae6d47ff56338
adam <bcuz@users.noreply.github.com>
2019-02-27 22:33:50 -0500

    footer stretch

---
+++ social-Card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+      <div className='container'>
+      <i class="far fa-comment fa-lg"></i>
+      <i class="fas fa-retweet fa-lg"> 6</i>
+      <i class="far fa-heart fa-lg"></i>
+      <i class="far fa-envelope fa-lg"></i>
+      </div>
+    </div>
+  )
+}
+
+export default Footer;
