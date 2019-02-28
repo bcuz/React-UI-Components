a5a4ec87d1aae49d3b142070199db99ddcfa3906
adam <bcuz@users.noreply.github.com>
2019-02-27 23:05:42 -0500

    less divs

--- social-Card/src/components/FooterComponents/Footer.js
+++ social-Card/src/components/FooterComponents/Footer.js
@@ -4,12 +4,10 @@ import './Footer.css';
 const Footer = () => {
   return (
     <div className='footer'>
-      <div className='container'>
       <i class="far fa-comment fa-lg"></i>
       <i class="fas fa-retweet fa-lg"> 6</i>
       <i class="far fa-heart fa-lg"></i>
       <i class="far fa-envelope fa-lg"></i>
-      </div>
     </div>
   )
 }
