f000b3e2a4d6f4982f95f2473515d117e48e9177
adam <bcuz@users.noreply.github.com>
2019-03-02 12:41:45 -0500

    CalculatorDisplay

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import NumberButton from './components/ButtonComponents/NumberButton';
 import ActionButton from './components/ButtonComponents/ActionButton';
+import CalculatorDisplay from './components/DisplayComponents/CalculatorDisplay';
 
 let keys = [String.fromCharCode(247), 7, 8, 9, "x", 4, 5, 6, String.fromCharCode(8212),  1, 2, 3, "+"]
 
 const App = () => {
   return (
     <div className="main">
+    <CalculatorDisplay />
       <ActionButton text='clear' />
       {keys.map(key => {
         return <NumberButton 
